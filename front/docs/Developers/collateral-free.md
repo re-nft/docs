51ceb0b1ca010a0233cd2dbd1f7d409e5c981e0f
Sean D <0xseen64@gmail.com>
2022-03-27 22:24:20 -0400

    feat: add initial content for reNFT docs

---
+++ front/docs/Developers/collateral-free.md
@@ -0,0 +1,4 @@
+# Collateral-Free Integration Guide
+
+![Collateral-Free Overview](../..//static/img/developers/21130_Collat-Free_InfoGraphic.png)
+
--- front/src/components/HomepageFeatures.js
+++ front/src/components/HomepageFeatures.js
@@ -1,43 +1,44 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './HomepageFeatures.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./HomepageFeatures.module.css";
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Purrrfect for Games",
+    Svg: require("../../static/img/cat.svg").default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Check our docs to integrate our collateral-free solution to enable
+        rentals in-game or for NFT gated events.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
+    title: "Gas-Efficient",
+    Svg: require("../../static/img/dragon.svg").default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Don't light your funds on fire with NFT wrapper solutions. Try our
+        gas-efficient collateralized and collateral-free registry rental
+        protocols.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
+    title: "Chain-Agnostic",
+    Svg: require("../../static/img/trees.svg").default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Lend and rent NFTs on Ethereum with collateral or collateral-free.
+        Polygon and Solana support coming soon.
       </>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} alt={title} />
       </div>
